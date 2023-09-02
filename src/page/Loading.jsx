005948a328b078adfd24d0e0d67e7d6311a86422
twosky0202 <leehaneul0202@naver.com>
2023-09-02 14:22:45 +0900

    fix: kakao login 수정

--- src/page/Loading.jsx
+++ src/page/Loading.jsx
@@ -1,34 +1,35 @@
-
-import React from "react";
-import axios from 'axios'; 
-
+import React, { useEffect } from "react";
+import axios from "axios";
 
 function Loading() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const code = urlParams.get("code");
+    console.log(code);
+
     console.log(window.location.href);
     // let ingacode = new URL(window.location.href).searchParams.get('code');
     // console.log(ingacode);
-    let kakaoUrl = new URL(window.location.href);
-    console.log(kakaoUrl);
+    // let kakaoUrl = new URL(window.location.href);
+    // console.log(kakaoUrl);
     // const ingastr = ingacode.toString();
     const requestBody = {
         // code: `${ingacode}`,
-        code: `${kakaoUrl}`,
+        code: code,
     };
-    axios('https://healody.shop/api/auth/kakao/callback2',{
-        data:requestBody,
-        method: 'GET',
+    axios("https://healody.shop/api/auth/kakao/callback2", {
+        data: requestBody,
+        method: "GET",
     })
-    .then(function(response) {
-        alert('로그인됐다임마')
-        console.log(response);
-    })
-    .catch(function(error) {
-        console.log('에러남');
-    })
-    return (
-        <>
-        </>
-    );
+        .then(function (response) {
+            alert("로그인됐다임마");
+            console.log(response);
+        })
+        .catch(function (error) {
+            console.log(error);
+            console.log("에러남");
+        });
+
+    return <></>;
 }
 
 export default Loading;
@@ -44,7 +45,7 @@ export default Loading;
 //                 url: `http://localhost:3000/loading/?code=${code}`,
 //                 headers: {
 //                     "Content-Type": "application/json;charset=utf-8", //json형태로 데이터를 보내겠다는뜻
-//                     "Access-Control-Allow-Origin": "*", 
+//                     "Access-Control-Allow-Origin": "*",
 //                   },
 //             }).then((res) => {
 //                 console.log(res);
